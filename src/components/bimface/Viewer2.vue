d683f859d937975ee055a2a4cf1b09108e2db7be
XiangJianfeng <i@xiangjianfeng.com>
2019-11-23 21:05:32 +0800

    update

--- src/components/bimface/Viewer2.vue
+++ src/components/bimface/Viewer2.vue
@@ -8,38 +8,60 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      fileId: this.$store.state.defStation.fileId,
       viewer3D: '',
       app: '',
       viewToken: ''
     }
   },
+  computed: {
+    fileId() {
+      return this.$store.state.defStation.fileId
+    }
+  },
   mounted() {
     //获取bimface viewtoken
-    this.getViewToken(this.fileId)
-
-    var loaderConfig = new BimfaceSDKLoaderConfig()
-    loaderConfig.viewToken = this.viewToken
-    BimfaceSDKLoader.load(
-      loaderConfig,
-      this.successCallback,
-      this.failureCallback
-    )
+    this.getViewToken()
   },
   methods: {
+    loadModel() {
+      var loaderConfig = new BimfaceSDKLoaderConfig()
+      loaderConfig.viewToken = this.viewToken
+      BimfaceSDKLoader.load(
+        loaderConfig,
+        this.successCallback,
+        this.failureCallback
+      )
+    },
     successCallback() {
       //获取DOM元素
       var domShow = document.getElementById('view')
       //创建WebApplication3DConfig
       var webAppConfig = new Glodon.Bimface.Application.WebApplication3DConfig()
+
+      webAppConfig.enableViewHouse = false //隐藏home图标
+      webAppConfig.Toolbars = ['MainToolbar']
+      webAppConfig.Buttons = [
+        'Home',
+        'RectangleSelect',
+        'Measure',
+        'Section',
+        'Walk',
+        'Map',
+        'Property',
+        'FullScreen'
+      ]
+
       //设置创建WebApplication3DConfig的dom元素值
       webAppConfig.domElement = domShow
       //创建WebApplication3D
       this.app = new Glodon.Bimface.Application.WebApplication3D(webAppConfig)
+
       //添加待显示的模型
       this.app.addView(this.viewToken)
+
       //获取viewer3D对象
       this.viewer3D = this.app.getViewer()
+      this.setBackgroundColor()
     },
     failureCallback(error) {
       console.log(error)
@@ -47,7 +69,7 @@ export default {
 
     //获取目录树
     getModelTree() {
-      this.viewer.getModelTree(objectdata => {
+      this.viewer3D.getModelTree(objectdata => {
         console.log(objectdata)
       })
     },
@@ -55,33 +77,35 @@ export default {
     //设置背景颜色
     setBackgroundColor() {
       var solidColor = new Glodon.Web.Graphics.Color(250, 250, 250, 0)
-      this.viewer.setBackgroundColor(solidColor)
-      // this.viewer.render();
+      this.viewer3D.setBackgroundColor(solidColor)
+      this.viewer3D.render()
     },
 
     //切换模型
     changeView() {},
 
     //获取bimface viewtoken
-    getViewToken(fileId) {
+    getViewToken(type = 'def') {
       axios
         .post('https://api.jue.sh/bimface/token', {
-          fileId: fileId
+          fileId: this.fileId
         })
         .then(res => {
           this.viewToken = res.data.viewToken
+
+          if (type == 'def') {
+            this.loadModel()
+          } else {
+            this.app.addView(this.viewToken)
+          }
         })
     }
   },
   watch: {
-    '$store.state.defStation.fileId': function() {
-      this.getViewToken(this.$store.state.defStation.fileId)
+    $route() {
+      this.viewer3D.removeView(this.viewToken)
+      this.getViewToken('change')
     }
-
-    // $route() {
-    //   console.log(this.$route.query.s)
-    //   this.changeView(this.$route.query.s)
-    // }
   }
 }
 </script>
